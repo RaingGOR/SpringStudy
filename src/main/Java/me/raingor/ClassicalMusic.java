09c7f0ae61657c3a1bbe5c84ebe572937decda9e
RainGor <kota-2019@inbox.ru>
2023-08-10 12:47:34 +0300

    initial  push les4

--- src/main/Java/me/raingor/ClassicalMusic.java
+++ src/main/Java/me/raingor/ClassicalMusic.java
@@ -1,8 +1,5 @@
 package me.raingor;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- src/main/Java/me/raingor/TestSpring.java
+++ src/main/Java/me/raingor/TestSpring.java
@@ -8,17 +8,11 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
+        Music music = context.getBean("musicBean", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
 
         musicPlayer.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-
-        classicalMusicPlayer.playMusic();
 
         context.close();
     }
